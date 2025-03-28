49e7d2392c620838e4bc7f8721c0e70caffa76c2
Naman Saraswat <namaniisc@gmail.com>
2025-03-28 07:40:06 +0530

    Implement post comment functionality with Socket.io
    Installed and configured socket.io
    
    Added logic to post comments and append them to the DOM
    
    Emitted events from the client for real-time updates
    
    Listened for incoming events on the client
    
    Implemented server-side event listening and broadcasting

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ const port = process.env.PORT || 3000
 
 app.use(express.static('public'))
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Listening on port ${port}`)
+})
+
+let io = require('socket.io')(server)
+
+io.on('connection', (socket) => {
+    console.log(`New connection: ${socket.id}`)
+    // Recieve event
+    socket.on('comment', (data) => {
+        data.time = Date()
+        socket.broadcast.emit('comment', data)
+    })
 })
